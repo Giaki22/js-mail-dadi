5c2752aad72efc35813edd11892ff23a8425cd94
Giaki22 <giaki22@hotmail.com>
2022-05-04 09:19:28 +0200

    + Fixed bug

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,16 +1,22 @@
-const mails = ["lisa@mail.com","homer@mail.com","marge@mail.com","bart@mail.com"]
+const mails = ["lisa@mail.com","homer@mail.com","marge@mail.com","bart@mail.com"];
+let s = false;
+
 document.addEventListener('keyup',
     function(event) {
         if(event.code === 'Space') {
             const umail = prompt("Verifica simpson. Inserisci la tua mail");
+            s = false;
             for (let i = 0; i < mails.length; i++) {
                 if (umail == mails[i]){
-                    alert("Accesso consentito");   
-                }
-                else if (umail !== mails[mails.length]) {
-                    alert("Accesso negato");
+                    s = true;
                 }
             }
+        }
+        if (s == true) {
+            alert("Accesso consentito");
+        }
+        else{
+            alert("Accesso negato");
         }
     }
 );            
